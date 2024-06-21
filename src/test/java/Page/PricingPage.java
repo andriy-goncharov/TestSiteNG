dde555b852c35d3f1db7e406d0657f7821caff29
andriy-goncharov <andergoncharov@gmail.com>
2024-06-21 20:12:48 +0300

    creating test for PricingPage

--- src/test/java/Page/PricingPage.java
+++ src/test/java/Page/PricingPage.java
@@ -1,13 +1,17 @@
-
-
 package Page;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
+
+import com.codeborne.selenide.*;
+import org.openqa.selenium.interactions.Actions;
+import org.underlab.commands.BtnProductVerificationNewWindow;
 
 import java.util.ArrayList;
 
+import static com.codeborne.selenide.Condition.clickable;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
+import static org.underlab.Constants.ConstantsLinks.CONTACT_URI;
+import static org.underlab.Constants.ConstantsLinks.LOGIN_URI;
 
 public class PricingPage extends BasePage {
 
@@ -20,7 +24,7 @@ public class PricingPage extends BasePage {
 
     private SelenideElement btnTryNowBasic = $("a[class^='btn-free-plan']");
     private SelenideElement btnTryBusinessPlan = $("#business-plan-try");
-    private SelenideElement btnContactSales = $("btn-enterprise-plan");
+    private SelenideElement btnContactSales = $("a[class^='btn-enterprise-plan']");
 
     public SelenideElement btnAllPricingPlans = $(".btn-all-plans");
 
@@ -50,32 +54,64 @@ public class PricingPage extends BasePage {
         return new PricingPage();
     }
 
-    public ArrayList checkSliderPlans500() {
+    public ArrayList checkSliderPlan500() {
         ArrayList<String> result = new ArrayList<>();
-        priceSlider.dragAndDrop(to(priceSliderMarks.get(6))).click();
+         priceSlider.click();
         result.add(fieldPlan.getText());
         result.add(fieldPriceYear.getText());
         result.add(fieldPriceMonth.getText());
         return result;
     }
-    public ArrayList checkSliderPlans1000() {
+    public ArrayList checkSliderPlan1000() {
         ArrayList<String> result = new ArrayList<>();
-        priceSlider.dragAndDrop(to(priceSliderMarks.last())).click();
+        int targetOffset = priceSliderMarks.get(0).getLocation().getX();
+        Actions actions = new Actions(WebDriverRunner.getWebDriver());
+        actions.clickAndHold(priceSlider)
+                .moveByOffset(targetOffset,0).release().perform();
+
         result.add(fieldPlan.getText());
         result.add(fieldPriceYear.getText());
         result.add(fieldPriceMonth.getText());
         return result;
     }
-    public ArrayList checkSliderPlans30() {
+    public ArrayList checkSliderPlan30() {
         ArrayList<String> result = new ArrayList<>();
-        priceSlider.dragAndDrop(to(priceSliderMarks.first())).click();
+        fieldPlan.click();
         result.add(fieldPlan.getText());
         result.add(fieldPriceYear.getText());
         result.add(fieldPriceMonth.getText());
         return result;
     }
-    public String testSignUP() {
-        return new String();
-//        return btnSignup.execute(new BtnProductVerificationNewWindow()).toString();
+    public String testSignUPBasic() {
+        btnTryNowBasic.scrollIntoView(false);
+        btnTryNowBasic.should(visible).should(clickable).click();
+        String pageTitle = switchTo().window(1).getTitle().toLowerCase();
+        webdriver().driver().url().contains(LOGIN_URI);
+        closeWindow();
+        switchTo().window(0);
+        return pageTitle;
+    }
+    public String testSignUPBusines() {
+        btnTryBusinessPlan.scrollIntoView(false);
+        btnTryBusinessPlan.should(visible).should(clickable).click();
+        String pageTitle = switchTo().window(1).getTitle().toLowerCase();
+        webdriver().driver().url().contains(LOGIN_URI);
+        closeWindow();
+        switchTo().window(0);
+        return pageTitle;
+    }
+    public String testBtnContactSales() {
+        btnContactSales.scrollIntoView(false);
+        btnContactSales.should(visible).should(clickable).click();
+        String pageTitle = switchTo().window(1).getTitle().toLowerCase();
+        webdriver().driver().url().contains(CONTACT_URI);
+        closeWindow();
+        switchTo().window(0);
+        return pageTitle;
+    }
+    public void testAllPricingPlans(){
+        btnAllPricingPlans.scrollIntoView(false);
+        btnAllPricingPlans.shouldBe(visible).shouldBe(clickable).click();
+        planBusines30.shouldBe(visible).shouldBe(clickable);
     }
 }
--- src/test/java/Test/PricingTest.java
+++ src/test/java/Test/PricingTest.java
@@ -1,5 +1,6 @@
 package Test;
 
+import Page.HomePage;
 import Page.PricingPage;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -8,7 +9,7 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.underlab.Constants.ConstantsLinks.*;
 
-public class PricengTest extends BaseTest {
+public class PricingTest extends BaseTest {
     @BeforeClass
     public void startPage() {
         open(PRICES_URI);
@@ -16,9 +17,15 @@ public class PricengTest extends BaseTest {
 
     @Test
     public void testBlockSlider() {
-        assertThat(PricingPage.getInstance().checkSliderPlans30()).contains("30","$240.00", "$25.00");
-        assertThat(PricingPage.getInstance().checkSliderPlans500()).contains("500","$960.00", "$100.00");
-        assertThat(PricingPage.getInstance().checkSliderPlans1000()).contains("1000+","$2400.00", "$250.00");
-
+        assertThat(PricingPage.getInstance().checkSliderPlan30()).contains("30","$240.00", "$25.00");
+        assertThat(PricingPage.getInstance().checkSliderPlan500()).contains("500","$960.00", "$100.00");
+        assertThat(PricingPage.getInstance().checkSliderPlan1000()).contains("1000+","$2400.00", "$250.00");
+    }
+    @Test
+    public void testBtnsPlans(){
+        assertThat(PricingPage.getInstance().testSignUPBasic()).contains(NAME_PAGE_PANEL);
+        assertThat(PricingPage.getInstance().testSignUPBusines()).contains(NAME_PAGE_PANEL);
+        assertThat(PricingPage.getInstance().testBtnContactSales()).contains(NAME_PAGE_CONTACT);
+        PricingPage.getInstance().testAllPricingPlans();
     }
 }
